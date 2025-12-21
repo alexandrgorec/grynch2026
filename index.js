f87e196af86423d60fb74c8058c832ba5b85b072
Alexandr_Goryaynov <2400sa@gmail.com>
2025-12-21 20:06:15 +0300

    first version without testing

--- index.js
+++ index.js
@@ -1,71 +1,130 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const fs = require('fs');
+const users = new Set();
 
-bot.start(async (ctx) => {
-    console.log("start");
+const grynchVerify = (message) => {
+    if (message.match(/гринч|ГРЫНЧ|grynch/gi))
+        return true;
+    else return false;
+}
+
+const snegovikVerify = (message) => {
+    if (message.match(/snegovik|снегов/gi))
+        return true;
+    else return false;
+}
+
+bot.start((ctx) => {
+    const userId = ctx.from.id;
+    users.delete(userId); // refresh user states
+    users.add(userId);
+    users.userId = {};
+    const user = users.userId;
+    user.state = "inputGrynch";
+    let startMessage = 'К нам на праздники приходит,\n'
+    startMessage += 'В шубу он одетый.\n'
+    startMessage += 'У него пустой мешок —\n'
+    startMessage += 'Ничего в нём нету!\n'
+    startMessage += 'Злой, зелёный и ужасный,\n'
+    startMessage += 'Неприятный, жутко страшный.\n'
+    startMessage += 'Ненавидит он детишек:\n'
+    startMessage += 'И девчонок, и мальчишек\n'
+    startMessage += 'На уме его одно —\n'
+    startMessage += 'Как похитить Рождество!\n'
+    startMessage += 'И, конечно, он не принц.\n'
+    startMessage += 'Одинокий, жуткий ...\n';
+    const photoStream = fs.createReadStream('.imgs/start.jpeg');
+    ctx.replyWithPhoto({ source: photoStream }, { caption: startMessage });
 });
 
+bot.on("message", ctx => {
+    let message = ctx.message.text;
+    const userId = ctx.from.id;
+    const user = users.userId;
 
 
 
-// bot.on('message', async (ctx) => {
-//     let message = ctx.message.text;
-//     const CHATID = ctx.message.chat.id;
-//     let user = await db.getUser(ctx.from.id);
-//     let room = await db.getRoom(user);
-//     const params = {
-//         user,
-//         ctx,
-//         message,
-//     }
-//     deleteBotMessages(user, CHATID);
-//     switch (message) {
-//         case commands.textMainMenu: { controller.mainMenu(params); break; }
-//         case commands.textRoom: { db.changeUserState(user, states.room); controller.room(params); break; }
-//     }
+    switch (user.state) {
+        case "inputGrynch": {
 
-//     switch (user.state) {
-//         case states.inputBuyerName: { // check inputed BuyerName
+            if (grynchVerify(message)) {
+                user.state = "waiting";
+                let replyText = "Кажется кто-то хотел себе новый зарядник? теперь он МОЙ, ахахАХАХА!!!"
+                // отправка видео steal.mp4
+                setTimeout(() => {
+                    const photoStream = fs.createReadStream('.imgs/olen.png');
+                    let replyMessage2 = 'Привет\n я генномодифицированый пес санты!'
+                    ctx.replyWithPhoto({ source: photoStream }, { caption: replyMessage2 });
+                }, 10000);
+                setTimeout(() => {
+                    ctx.reply("Хотя кого я обманываю, я просто олень!")
+                }, 13000);
+                setTimeout(() => {
+                    const keyboardMarkup = {
+                        reply_markup: {
+                            keyboard: [["Поймаем засранца 🐾"]],
+                            resize_keyboard: true, // Optional: makes the keyboard smaller
+                            one_time_keyboard: true // Optional: keeps the keyboard after a button is pressed
+                        }
+                    };
+                    ctx.reply("Гринч оставил следы! я могу его выследить! для этого мне понадобится твоя помощь!\n тебе нужно будет решить две простые задачки, поспеши!", keyboardMarkup)
+                }, 16500);
+            }
+            else {
+                ctx.reply("Ответ не верный! попробуй еще раз!")
+            }
+            break;
+        }
+        case "inputSnegovik": {
+            if (snegovikVerify(message)) {
+                user.state = "waiting";
+                let message = "Отлично, я вышел на след, Гринч спрятал подарок в этом доме!";
+                const photoStream = fs.createReadStream('.imgs/dom.png');
+                ctx.replyWithPhoto({ source: photoStream }, { caption: message });
+                setTimeout(() => {
+                    ctx.reply("Вторая задачка и последняя. Гринч немного поколдавал, заметая следы - он оставил тебе только ОДНУ попытку отгадать задачку, СОБЕРИСЬ!")
+                }, 5000);
+                setTimeout(() => {
+                    const photoStream = fs.createReadStream('.imgs/zadacha2.jpg');
+                    ctx.replyWithPhoto({ source: photoStream }, { caption: "Введите правильный ответ, у вас ОДНА попытка!" });
+                }, 10000);
+                setTimeout(() => {
+                    // отправка видео где гринч ржет
+                    user.state = 'inputRandom';
+                }, 12500);
+            } else {
+                ctx.reply("Ответ не верный! попробуй еще раз!")
+            }
+            break;
+        }
+        case "inputRandom": {
+            ctx.reply("Слава богу Гринч тупой и сам не знает ответ на эту загадку!\n Я могу найти твой подарок, но мне нужно чтобы ты снял магию Гринча, ПОЙ песню Дискотеки аварии - Новый год к нам МЧИТСЯ, СКОРЕЕ, ОТВЛЕКАЙ ЕГО!")
+            setTimeout(() => {
+                const photoStream = fs.createReadStream('.imgs/mesto.jpg');
+                ctx.replyWithPhoto({ source: photoStream }, { caption: "Отлично сработано, я нашел подарок! ВОТ ОН!!!" });
+            }, 15000);
+            break;
+        }
 
-//             break;
-//         }
-//         //         case states.inputBuySum: {
-//         //             if ((Number.isFinite(+message)) && (+message > 0) && (+message < 10000000)) {
-//         //                 user.buy.cost = +message;
-//         //                 user.state = states.inputBuyDescription;
-//         //                 menuKeyboard.reply_markup.keyboard = [[commands.textRoom]];
-//         //                 reply('На что потратил? Дай краткое описание расходу:', menuKeyboard, ctx, user)
+    }
 
-//         //             } else {
-//         //                 menuKeyboard.reply_markup.keyboard = [[commands.textRoom]];
-//         //                 reply('Вводи нормальное число, без всякой херни:', menuKeyboard, ctx, user)
-//         //             }
-
-//         //             break;
-//         //         }
-//         //         case states.inputBuyDescription: {
-//         //             user.buy.description = message;
-//         //             menu.reply_markup.inline_keyboard = [];
-//         //             if (user.activeRoom) {
-//         //                 user.activeRoom.users.forEach((name, ind) => menu.reply_markup.inline_keyboard.push([{ text: name, callback_data: JSON.stringify({ command: commands.chooseBuyer, data: ind }) }]));
-//         //                 menu.reply_markup.inline_keyboard.push([buttons.buys]);
-//         //                 reply('Кто из участников платил?', menu, ctx, user, ctx, user)
-//         //             }
-//         //             else {
-//         //                 menu.reply_markup.inline_keyboard = mainMenu;
-//         //                 reply('Выберите пункт меню:', menu, ctx, user, ctx, user)
-//         //             }
-//         //             break;
-//         //         }
-//     }
-//     ctx.deleteMessage();
-// });
+    switch (message) {
+        case "Поймаем засранца 🐾": {
+            let zagadka = "";
+            zagadka += 'Во дворе стоит мужик,\n'
+            zagadka += 'К зимним холодам привык,\n'
+            zagadka += 'Белый - белый и холодный,\n'
+            zagadka += 'Не женатый он, свободный\n'
+            zagadka += 'И с ведром на голове,\n'
+            zagadka += 'Что за тип, скажите мне ?\n'
+            ctx.reply(zagadka);
+            user.state = "inputSnegovik";
+            break;
+        }
+    }
+})
 
 bot.launch()
-  .then(() => {
-    console.log('Бот успешно запущен!');
-  })
-  .catch((err) => {
-    console.error('Ошибка при запуске бота:', err);
-  });
+
